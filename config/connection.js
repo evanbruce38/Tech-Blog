023645fffb67844acb51e38f51447e019bc96e5c
evanbruce38 <106777653+evanbruce38@users.noreply.github.com>
2022-09-13 16:50:22 -0400

    created db:

--- config/connection.js
+++ config/connection.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 // connection to db
 const sequelize = process.env.JAWSDB_URL
 ? new Sequelize(process.env.JAWSDB_URL)
-: new Sequelize('techblog_db', 'root', 'Rebels38?' {
+: new Sequelize('techblog_db', 'root', 'Rebels38?', {
     host: 'localhost',
     dialect: 'mysql',
     port: 3306
